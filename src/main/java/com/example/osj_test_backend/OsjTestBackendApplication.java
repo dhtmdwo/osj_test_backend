0d2cf91a03bbdd1cf6c9514f199520592f2422c7
seungjae <dhtmdwo73@naver.com>
2025-02-26 09:20:13 +0900

    second commit

---
+++ src/main/java/com/example/osj_test_backend/OsjTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.osj_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsjTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsjTestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/osj_test_backend/OsjTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osj_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsjTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
